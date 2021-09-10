632268036a907829abb70d27538d9aaf86b9d28f
Hao Jie <aspiringcatlover@gmail.com>
2021-09-10 15:17:57 +0800

    added data.tsv

---
+++ src/record.h
@@ -0,0 +1,26 @@
+#ifndef RECORD_H
+#define RECORD_H
+
+//to use string for tconst
+#include <iostream>
+using namespace std;
+
+/*
+Data structure representing a movie record stored in blocks in the database.
+There are 3 columns:
+tconst (String)
+averageRating (float)
+numVotes (int)
+
+*/
+struct Record
+{
+    
+
+
+
+  
+}
+
+
+#endif
