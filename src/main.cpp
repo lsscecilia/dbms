96be1ea94d6ed22668233bde57b7af9e2008c178
lsscecilia <lsscecilia8888@gmail.com>
2021-09-28 19:35:20 +0800

    integrate (halfway)

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,19 @@
 #include "record.h"
 #include "block.h"
 #include "storage.h"
+#include "bplustree.h"
+
 using std::vector;
 using namespace std;
 
 int main()
 {
 
-    int block_size;
+    int blockSize;
 
     //Read Input File
     ifstream infile;
-    infile.open("data.tsv");
+    infile.open("../data/data.tsv");
     if (!infile)
     {
         cout << "Error in reading the file" << endl; //show error if can't read file
@@ -29,23 +31,38 @@ int main()
 
     //Initialize the block size based on input
     cout << "Enter Block Size" << endl;
-    cin >> block_size;
+    cin >> blockSize;
 
-    cout << "Building Storage with Block Size of " << block_size << " bytes...." <<  endl;
+    cout << "Building Storage with Block Size of " << blockSize << " bytes...." <<  endl;
 
     //Declaring and Initializing the variables
 
     string row;
     Storage s;
-    Block blk;
+    
     Record r;
-    int max_records_in_block = floor(block_size/r.getRecordSize());
+    int max_records_in_block = floor(blockSize/r.getRecordSize());
+
+	Block blk(max_records_in_block);
     s.blocks.push_back(blk);
-    int block_count = s.getNumBlocks()-1;
+    int blockCount = s.getNumBlocks()-1;
+    cout << "init block count: " << blockCount << endl;
+
+    // get parameter N for bplustree
+    int sizeOfOtherDataInNode = sizeof(bool) + sizeof(uint32_t);
+    int sizeOfKey = sizeof(float) + sizeof(Pointer);
+
+    cout << "size of other shit in node: " << sizeOfOtherDataInNode << endl;
+    cout << "size of key: " << sizeOfKey << endl; 
+
+    int maxNumKeyInNode = floor((blockSize - sizeOfOtherDataInNode - sizeof(Pointer)) / sizeOfKey);
+    cout << "max num key in node: " << maxNumKeyInNode << endl;
+
+    BPlusTree bplustree(maxNumKeyInNode);
 
     //Skip Header
     //getline(infile, row);
-
+    std::shared_ptr<Block> blockPtr;
     //For all lines in the file
     while(getline(infile, row)) {
         vector <string> tokens;
@@ -65,25 +82,26 @@ int main()
 
         //If there is space in current block, add record to this block else create a new block and and record to the new block
 
-        //Max number of records in block shouldn't exceed floor of BLOCK_SIZE / RECORD_SIZE
-        if(s.blocks[block_count].getNumRecords() < max_records_in_block)
+        //Max number of records in block shouldn't exceed floor of BLOCKSize / RECORD_SIZE
+        if(s.blocks[blockCount].getNumRecords() < max_records_in_block)
         {
-            s.blocks[block_count].addRecord(r);//push a record r to block b
-            //cout << "block " << block_count << " Num Records:" << s.blocks[block_count].getNumRecords() << endl;
-            //cout<< "Current Block Size: " << s.blocks[block_count].getBlockSize() << endl;
+            s.blocks[blockCount].addRecord(r);//push a record r to block b
+            //cout << "block " << blockCount << " Num Records:" << s.blocks[blockCount].getNumRecords() << endl;
+            //cout<< "Current Block Size: " << s.blocks[blockCount].getBlockSize() << endl;
         }
         else
         {
-            //cout<< "Number Records " << s.blocks[block_count].getNumRecords() << endl;
-            //cout<< "Ending Block Size: " << s.blocks[block_count].getBlockSize() << endl;
-            block_count++;//increment block counter
-            Block blk; //create a new block
-            s.addBlock(blk);
-            s.blocks[block_count].addRecord(r);//push a record r to new block b
-            //cout << "new_block " << block_count << " Num Records:" << s.blocks[block_count].getNumRecords() << endl;
+            //cout<< "Number Records " << s.blocks[blockCount].getNumRecords() << endl;
+            //cout<< "Ending Block Size: " << s.blocks[blockCount].getBlockSize() << endl;
+            blockCount++;//increment block counter
+            blockPtr = std::make_shared<Block>();
+            s.addBlock(*blockPtr.get());
+            s.blocks[blockCount].addRecord(r);//push a record r to new block b
+            //cout << "new_block " << blockCount << " Num Records:" << s.blocks[blockCount].getNumRecords() << endl;
         };
 
-
+        // insert into bplus tree
+        // bplustree.InsertNode(r.numVotes, blockPtr);
     }
 
     infile.close();//close the file after finish reading
@@ -91,7 +109,7 @@ int main()
     cout <<"Number of Records: " << s.getNumRecords() << endl; //output the final number of blocks for a given block size
     cout <<"Size of Record: " << r.getRecordSize() << " bytes" << endl;
     cout <<"Number of Blocks: " << s.getNumBlocks() << endl;
-    cout <<"Size of Block: " << block_size << " bytes" << endl;
+    cout <<"Size of Block: " << blockSize << " bytes" << endl;
     cout <<"Max Records in Block: " << max_records_in_block << " Records" << endl;
     cout <<"Min Records in Block: " << s.blocks[s.getNumBlocks()-1].getNumRecords() << " Records" << endl;
     cout <<"Total Size of Storage: " << ((double)s.getStorageSize() / 1024000)<< " Mb" << endl;
@@ -99,9 +117,14 @@ int main()
     cout <<"Building B+ Tree Index on Average Rating" << endl;
 
 
-/*    class BPlusTree tree;
+    Pointer ptr;
+    float random = 10;
+    cout << "size of pointer: " << sizeof(ptr) << endl;
+    cout << "size of float: " << sizeof(random) << endl; 
+    cout << "bool: " << sizeof(bool) << endl;
+    cout << "uint32_t: " << sizeof(uint32_t) << endl;
 
-    int maxNumKeys =  floor((block_size-4)/8); //Parameter N
+    int maxNumKeys =  floor((blockSize-4)/8); //Parameter N
     int min_internal = floor(maxNumKeys/2);
     int min_leaf = floor((maxNumKeys+1)/2);
 
@@ -117,43 +140,46 @@ int main()
         }
     }
 
-    tree.Initialize(maxNumKeys);
-
-    tree.Open_Output_File();
-    vector <Block *> block_ptrs;
-    for (int i = 0; i < s.getNumBlocks();i++){
-        struct Block *block_ptr;
-        block_ptr = &s.blocks[i];
-        block_ptrs.push_back(block_ptr);
-        std::ostringstream address;
-        address << (void const *)block_ptr;
-        std:string name = address.str();
-
-        for(int j = 0; j < block_ptr->getNumRecords(); j++){
-            tree.Insert(float(block_ptr->records[j].numVotes), i);
-
-        }
-
-    }
-    cout << "B+ Tree has been created" << endl;
-    tree.Print_Tree();
-
-    cout <<"--------------------------" << endl;
-    cout << "Searching Records with Average Rating = 8" << endl;
-    tree.Search(8,s);
-
-    cout <<"--------------------------" << endl;
-    cout << "Searching Records with Average Rating from 7 - 9" << endl;
-
-    tree.Search(7.0,9.0,s);
-
-    cout <<"--------------------------" << endl;
-    cout << "Deleting Records with Average Rating = 7" << endl;
-    tree.Search_Delete(7.0,s);
-    tree.Update_Tree(s,20);
 
 
-    tree.Close_Output_File();
+    
+    // bplustree.Initialize(maxNumKeys);
 
-    return 1;*/
+    // bplustree.Open_Output_File();
+    // vector <Block *> block_ptrs;
+    // for (int i = 0; i < s.getNumBlocks();i++){
+    //     struct Block *block_ptr;
+    //     block_ptr = &s.blocks[i];
+    //     block_ptrs.push_back(block_ptr);
+    //     std::ostringstream address;
+    //     address << (void const *)block_ptr;
+    //     std:string name = address.str();
+
+    //     for(int j = 0; j < block_ptr->getNumRecords(); j++){
+    //         bplustree.Insert(float(block_ptr->records[j].numVotes), i);
+
+    //     }
+
+    // }
+    // cout << "B+ Tree has been created" << endl;
+    // bplustree.Print_Tree();
+
+    // cout <<"--------------------------" << endl;
+    // cout << "Searching Records with Average Rating = 8" << endl;
+    // bplustree.Search(8,s);
+
+    // cout <<"--------------------------" << endl;
+    // cout << "Searching Records with Average Rating from 7 - 9" << endl;
+
+    // bplustree.Search(7.0,9.0,s);
+
+    // cout <<"--------------------------" << endl;
+    // cout << "Deleting Records with Average Rating = 7" << endl;
+    // bplustree.Search_Delete(7.0,s);
+    // bplustree.Update_Tree(s,20);
+
+
+    // bplustree.Close_Output_File();
+
+    // return 1;
 }
